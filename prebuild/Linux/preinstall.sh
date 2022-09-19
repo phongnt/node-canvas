c5c0c7d132aa1102cb3c62c792042ed4aaa588c7
phongnt <genycopedison@gmail.com>
2022-09-19 16:45:26 +0700

    Prebuild for Linux

--- prebuild/Linux/preinstall.sh
+++ prebuild/Linux/preinstall.sh
@@ -1,7 +1,7 @@
 # apt-get-style dependencies aren't done here since the
 # linux build is done on a docker image that has them
 
-git clone git://anongit.gentoo.org/proj/pax-utils.git
+git clone -b v1.3.4 git://anongit.gentoo.org/proj/pax-utils.git
 cd pax-utils
 PATH=$PATH:$PWD
 make
